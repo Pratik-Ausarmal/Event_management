ecda233063405add9fb629f3aa60071954973b56
Pratik-Ausarmal <maccpratik@gmail.com>
2026-01-20 11:11:28 +0530

    Change database connection to production server
    Updated database connection details for production.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "pratik@2004",
-  database: "event_management",
+  host: "bmmzroy3fnqx9pikmwtn-mysql.services.clever-cloud.com",
+  user: "upgnnzwbbcy1yeqb",
+  password: "DI6PdcgsRdfYC1JTiBDW",
+  database: "bmmzroy3fnqx9pikmwtn",
   multipleStatements: true   // ✅ ADD THIS LINE
 });
